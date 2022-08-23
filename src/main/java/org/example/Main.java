4a03c80e73db3b6d903aeae4d0e2b19e297b9ba8
RR <rebecca.rivera@awacademy.training>
2022-08-23 11:23:04 +0200

    Obstacle dead for real

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,37 +6,6 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 import java.util.ArrayList;
 import java.util.List;
-/*
-        RUSH
-        Lvl 1
-        * Hinder kommer och går mot en
-
-        Står lvl 2
-        * Hinder
-        * + 1 Monster jagar
-
-        * Lvl 3
-        * Hinder ( längre + snabbare)
-        * 1 monster jagar
-
-        Lvl 4
-        * Hinder ( längre + bredare+ snabbare)
-        * 2 monster jaguar
-        * 5 bomber kommer långsamt
-
-        Lvl 5
-        * samma Hinder som lvl 3
-        * 3 monster jagar
-        * 10 bomber lite snabbare
-
-        Extra finesser
-        Dör av att röra taket och botten
-        Hinder en färg (olika färg för lvl?)
-        Bomber en färg
-        Monster en färg eller symbol
-        Spelare blir röd av död
-        Game Over när man dör
-        Spel meny med 2 val - starta eller exit*/
 public class Main {
     public static void main(String[] args) throws Exception {
         TerminalSize ts = new TerminalSize(60,15);
@@ -53,6 +22,7 @@ public class Main {
         terminal.setCursorPosition(player.x, player.y);
         terminal.putCharacter(playerCharacter);
 
+       // printNewObstacles
         // Create obstacles array
         Position[] obstacles = new Position[10];
         for(int i = 0;i<10;i++){
@@ -86,16 +56,16 @@ public class Main {
 
             switch (keyStroke.getKeyType()) {
                 case ArrowDown:
-                    player.y += 2;
+                    player.y += 1;
                     break;
                 case ArrowUp:
-                    player.y -= 2;
+                    player.y -= 1;
                     break;
                 case ArrowRight:
-                    player.x += 2;
+                    player.x += 1;
                     break;
                 case ArrowLeft:
-                    player.x -= 2;
+                    player.x -= 1;
                     break;
             }
 
