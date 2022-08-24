05c35f9184d1c21ba106a18dfe566c586424a9e0
Harris Novljakovic <harrisnovljakovic@gmail.com>
2022-08-24 10:17:41 +0200

    monster kod i botten

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -96,7 +96,7 @@ public class Main {
                         break;
                     }
                 }
-
+                handleMonsters(player,terminal);
 
                 Thread.sleep(5);
                 keyStroke = terminal.pollInput();
@@ -180,5 +180,32 @@ public class Main {
         return true;
     }
 
+    public static void handleMonsters (Position player, Terminal terminal) throws IOException {
+        List<Position> monsters = new ArrayList<>();
+        monsters.add(new Position(3, 3));
+        monsters.add(new Position(23, 23));
+        monsters.add(new Position(23, 3));
+        monsters.add(new Position(3, 23));
+
+        for (Position monster : monsters) {
+            terminal.setCursorPosition(monster.x, monster.y);
+            terminal.putCharacter(' ');
+
+            if (player.x > monster.x) {
+                monster.x++;
+            } else if (player.x < monster.x) {
+                monster.x--;
+            }
+            if (player.y > monster.y) {
+                monster.y++;
+            } else if (player.y < monster.y) {
+                monster.y--;
+            }
+
+            terminal.setCursorPosition(monster.x, monster.y);
+            terminal.putCharacter('X');
+        }
+
+    }
 
 }
