0781fc5d4db988893beaa8990f15a999fab8913c
Harris Novljakovic <harrisnovljakovic@gmail.com>
2022-08-24 11:06:42 +0200

    bomb ändringar

--- src/main/java/org/example/Bombs.java
+++ src/main/java/org/example/Bombs.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import com.googlecode.lanterna.terminal.Terminal;
 import java.util.Random;
 import java.io.IOException;
@@ -5,19 +7,20 @@ import java.io.IOException;
 public class Bombs {
     public static int x;
     public static int y;
+    public Terminal terminal;
 
-    public Bombs(int x, int y){
+    public Bombs(int x, int y, Terminal terminal){
         this.x = x;
         this.y = y;
+        this.terminal = terminal;
     }
 
-    public void createBombs(int Bombs, int x, int y) throws IOException {
+    public void createBombs() throws IOException {
          Random r1 = new Random();
-         x = r1.nextInt(15);
-         y = r1.nextInt(15);
+         this.x = r1.nextInt(15);
+         this.y = r1.nextInt(15);
 
         final char bomb = 'O';
-        Terminal terminal = null;
         terminal.setCursorPosition(x, y);
         terminal.putCharacter(bomb);
         terminal.flush();
