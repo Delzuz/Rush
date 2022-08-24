ec2dc67846037a21e90124ee7decb1213577cf8c
Harris Novljakovic <harrisnovljakovic@gmail.com>
2022-08-24 11:20:34 +0200

    clean up

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,37 +11,6 @@ import java.util.Random;
 
 import static com.googlecode.lanterna.input.KeyType.Escape;
 
-/*
-        RUSH
-        Lvl 1
-        * Hinder kommer och går mot en
-
-        Står lvl 2
-        * Hinder
-        * + 1 Monster jagar
-
-        * Lvl 3
-        * Hinder (längre + snabbare)
-        * 1 monster jagar
-
-        Lvl 4
-        * Hinder (längre + bredare+ snabbare)
-        * 2 monster jaguar
-        * 5 bomber kommer långsamt
-
-        Lvl 5
-        * samma Hinder som lvl 3
-        * 3 monster jagar
-        * 10 bomber lite snabbare
-
-        Extra finesser
-        Dör av att röra taket och botten
-        Hinder en färg (olika färg för lvl?)
-        Bomber en färg
-        Monster en färg eller symbol
-        Spelare blir röd av död
-        Game Over när man dör
-        Spel meny med 2 val - starta eller exit*/
 public class Main {
 
     public static void main(String[] args) throws Exception {
@@ -83,6 +52,7 @@ public class Main {
 
             do {
                 index+=5;
+                //First set of moving obstacles
                 if (index % 100==0) {
                     continueReadingInput = handleObstacles1(obstacles1, player, terminal);
                     if (!continueReadingInput) {
@@ -90,7 +60,7 @@ public class Main {
                         break;
                     }
                 }
-
+                //Second set of moving obstacles
                 if (index % 100==0) {
                     continueReadingInput = handleObstacles2(obstacles2, player, terminal);
                     if (!continueReadingInput) {
